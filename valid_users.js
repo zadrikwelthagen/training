b8790d1aaada1488147576d881a195bd571be065
zadrikwelthagen <zadrik.welthagen@gmail.com>
2026-06-24 13:23:04 +0200

    Add files via upload

---
+++ valid_users.js
@@ -0,0 +1,5 @@
+const validUsers = {
+  "CSFG123": { name: "Zadrik", surname: "Welthagen" },
+  "CSFG346": { name: "Carel", surname: "Jansen van Rensburg" },
+  "CSFG789": { name: "Markus", surname: "Welthagen" }
+};
